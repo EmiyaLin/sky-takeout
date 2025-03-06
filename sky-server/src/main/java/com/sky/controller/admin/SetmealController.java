3ab25f04f3d21d3fe2cbcff84a81795fc1907ba8
EmiyaLin <1259449968@qq.com>
2025-03-06 17:32:08 +0800

    day04 - 套餐分页查询功能

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,8 +1,12 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -24,5 +28,13 @@ public class SetmealController {
         log.info("新增套餐：{}", setmealDTO);
         setmealService.save(setmealDTO);
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        log.info("套餐分页查询：{}", setmealPageQueryDTO);
+        PageResult result = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(result);
     }
 }
